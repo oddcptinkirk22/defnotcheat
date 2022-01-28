2d964ad2bf6f6a6b7cee4e158b112a35b5daeeeb
Otternutz <42265006+Otternutz@users.noreply.github.com>
2022-01-27 21:45:27 -0600

    Update Aimbot.cpp

--- Aimbot.cpp
+++ Aimbot.cpp
@@ -6,10 +6,26 @@ extern int Hight;
 
 
 
-float get() {
+float get2DDistance (D3DXVECTOR2 a, D3DXVECTOR2 b) {
 
 
+	return sqrt(powf(a.x - b.x, 2) + powf(a.y - b.y, 2));
 
-	return sqrt(a.x - b.x, 2) + (a.y - b.y, 2));
+}
 
+bool inFOV(double x, double y)
+{
+	if (get2DDistance(D3DXVECTOR2(Width / 2, Height / 2), D3DXVECTOR2(x, y)) < values.aimbotFOV) {
+
+		return true;
+
+	}
+	else
+		return false
+
+}
+
+bool HasFlag(int flag, int state)
+{
+	return (state & (int)flag) == (int)flag;
 }
