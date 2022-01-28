d84c105078ca139aee066f67a5f22d08e19d35ea
Otternutz <42265006+Otternutz@users.noreply.github.com>
2022-01-27 22:06:41 -0600

    Update Aimbot.cpp

--- Aimbot.cpp
+++ Aimbot.cpp
@@ -32,6 +32,38 @@ bool HasFlag(int flag, int state)
 
 bool isTargetableEntity(Entity player) {
 	BYTE State;
-	if (player
-		)
+	if (player.health < -0)
+			return false;
+	if (player.position, entity[0].position) > values.aimbotDistance)
+			return false;
+
+	if (HasFlag(0x8, player.state))
+	{
+		return false;
+	}
+	return true;
+}
+
+void aimAtPlayer(Entity Player) {
+	D3DXVECTOR2 toWrite = { 0,0 };
+	D3DXVECTOR3 headpos = { 0,0,0 };
+	D3DXVECTOR3 headpos = { 0,0,0 };
+
+
+	while ((GetAsyncKeyState(0x46) & 1 || GetAsyncKeyState(0x46)))
+	{
+		head pos = read<D3DVECTOR3>(player.visualstate + 0x90);
+		BOOL ducking = hasFlag(1, player.state);
+		if (ducking)
+			headpos.y += 0.91;
+		else
+			headpos.y += 1.52;
+		toWrite = calcmyangles(&entity[0].position, &headpos);
+		write<D3DXVECTOR2>(entity[2].playerInput + 0x44, toWrite);
+		if (!istargetableEntity(player)) {
+			Sleep(100);
+			break;
+		}
+		Sleep(2);
+	}
 }
